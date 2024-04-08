104de94477679bbbd65c9c0fd9757f6f7168271d
emsthf <deunsol916@gmail.com>
2024-04-08 15:08:20 +0900

    feat: 자동화 테스트 - 경계값 테스트 - 테스트 케이스는 항상 해피 케이스만 작성해선 안된다. - 해피 케이스와 예외 케이스를 모두 작성해 경계값 테스트를 해야 함! 성공과 실패의 경계선을 모두 테스트(범위, 날짜, 구간 등)

--- src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
+++ src/test/java/sample/cafekiosk/unit/CafeKioskTest.java
@@ -5,6 +5,7 @@ import sample.cafekiosk.unit.beverage.Americano;
 import sample.cafekiosk.unit.beverage.Latte;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CafeKioskTest {
 
@@ -32,6 +33,38 @@ class CafeKioskTest {
         // then
         assertThat(cafeKiosk.getBeverages()).hasSize(1);
         assertThat(cafeKiosk.getBeverages().get(0).getName()).isEqualTo("아메리카노");
+    }
+
+    /**
+     * 음료 수 추가 경계값 해피 케이스 테스트
+     */
+    @Test
+    void addSeveralBeverages() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        // when
+        cafeKiosk.add(americano, 2);
+
+        // then
+        assertThat(cafeKiosk.getBeverages().get(0)).isEqualTo(americano);
+        assertThat(cafeKiosk.getBeverages().get(1)).isEqualTo(americano);
+    }
+
+    /**
+     * 음료 수 추가 경계값 예외 케이스 테스트
+     */
+    @Test
+    void addZeroBeverages() {
+        // given
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        Americano americano = new Americano();
+
+        // when & then
+        assertThatThrownBy(() -> cafeKiosk.add(americano, 0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음료 추가 개수는 1개 이상이어야 합니다.");
     }
 
     @Test
