c2d1e45cf732c42eb38dce95dc9fcabf2aa758c0
emsthf <deunsol916@gmail.com>
2024-04-12 15:08:33 +0900

    feat: business layer test2 - 상품 등록 서비스 로직 TDD로 작성 - @Transactional의 readOnly 옵션을 적절하게 쓰면 성능 향상을 할 수 있다. 기본적으로 서비스 클래스에 @Transactional(readOnly = true)를 사용하고 CUR이 필요한 곳에 @Transactional을 열어주자. - 더 나아가서 Command와 Query를 분리한 CQRS 패턴으로 구현하면 장애 격리에 더욱 좋다.

--- src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/ProductRepository.java
@@ -1,6 +1,7 @@
 package sample.cafekiosk.spring.domain.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +17,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllBySellingStatusIn(List<ProductSellingStatus> sellingTypes);
 
     List<Product> findAllByProductNumberIn(List<String> productNumbers);
+
+    @Query(value = "SELECT p.product_number FROM product p ORDER BY p.id DESC LIMIT 1", nativeQuery = true)
+    String findLatestProductNumber();
 }
